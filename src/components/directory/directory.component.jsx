c426f927a8e4ecaaca771159d048656bfa7e2312
Nguyen Dinh Phu <nguyendinhphu98@gmail.com>
2021-08-06 11:35:26 +0700

    Update menu-items and ReactRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -40,13 +40,8 @@ function Directory() {
 
 	return (
 		<div className="directory-menu">
-			{sections.map(({ title, id, imageUrl, size }) => (
-				<MenuItem
-					key={id}
-					title={title}
-					imageUrl={imageUrl}
-					size={size}
-				/>
+			{sections.map(({ id, ...otherSectionProps }) => (
+				<MenuItem key={id} {...otherSectionProps} />
 			))}
 		</div>
 	);
