a44c0b2ec91eaf53495ad3dbde19bba3e6e3b036
Nguyen Dinh Phu <nguyendinhphu98@gmail.com>
2021-08-14 23:39:21 +0700

    Add ReduxPersist

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import "./index.css";
 import App from "./App";
 import configureStore from "./store/configureStore";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
 
 const store = configureStore();
+let persistor = persistStore(store);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<React.StrictMode>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</React.StrictMode>
+		<PersistGate loading={null} persistor={persistor}>
+			<React.StrictMode>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</React.StrictMode>
+		</PersistGate>
 	</Provider>,
 	document.getElementById("root")
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,20 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./root";
 import logger from "redux-logger";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export default function () {
 	return configureStore({
-		reducer,
+		reducer: persistedReducer,
 		middleware: [
 			...getDefaultMiddleware({
 				serializableCheck: false,
